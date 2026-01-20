04c66960cfb1a6b75a012e7d55db47231b500075
Jens M. <jensmayer89@gmail.com>
2026-01-20 14:42:20 +0100

    Improve screen reader settings for buttons

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -4,29 +4,39 @@ import 'package:flutter/material.dart';
 // Class for the main button of the app e.g. settings or profile
 class AppButton extends StatelessWidget {
   final String text;
+  final String semanticsLabel;
   final VoidCallback onPressed;
   final IconData? icon;
 
   const AppButton({
     super.key,
     required this.text,
+    required this.semanticsLabel,
     required this.onPressed,
-    required this.icon,
+    this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: icon != null
-          ? Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text(text), Icon(icon, size: 30)],
-              ),
-            )
-          : Text(text),
+    return Semantics(
+      label: semanticsLabel,
+      button: true,
+      excludeSemantics: true,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: icon != null
+            ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(text),
+                    ExcludeSemantics(child: Icon(icon, size: 30)),
+                  ],
+                ),
+              )
+            : Text(text),
+      ),
     );
   }
 }
@@ -34,36 +44,46 @@ class AppButton extends StatelessWidget {
 // Class for the primary button e.g. save or submit
 class PrimaryButton extends StatelessWidget {
   final String text;
+  final String semanticsLabel;
   final VoidCallback onPressed;
   final IconData? icon;
 
   const PrimaryButton({
     super.key,
     required this.text,
+    required this.semanticsLabel,
     required this.onPressed,
-    required this.icon,
+    this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
     final baseStyle = Theme.of(context).elevatedButtonTheme.style;
 
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: baseStyle?.copyWith(
-        backgroundColor: WidgetStateProperty.all(
-          const Color.fromRGBO(9, 72, 72, 1.0),
+    return Semantics(
+      label: semanticsLabel,
+      button: true,
+      excludeSemantics: true,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: baseStyle?.copyWith(
+          backgroundColor: WidgetStateProperty.all(
+            const Color.fromRGBO(9, 72, 72, 1.0),
+          ),
         ),
+        child: icon != null
+            ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(text),
+                    ExcludeSemantics(child: Icon(icon, size: 30)),
+                  ],
+                ),
+              )
+            : Text(text),
       ),
-      child: icon != null
-          ? Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text(text), Icon(icon, size: 30)],
-              ),
-            )
-          : Text(text),
     );
   }
 }
@@ -71,36 +91,46 @@ class PrimaryButton extends StatelessWidget {
 // Class for secondary button e.g. for delete or cancel
 class SecondaryButton extends StatelessWidget {
   final String text;
+  final String semanticsLabel;
   final VoidCallback onPressed;
   final IconData? icon;
 
   const SecondaryButton({
     super.key,
     required this.text,
+    required this.semanticsLabel,
     required this.onPressed,
-    required this.icon,
+    this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
     final baseStyle = Theme.of(context).elevatedButtonTheme.style;
 
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: baseStyle?.copyWith(
-        backgroundColor: WidgetStateProperty.all(
-          Color.fromRGBO(46, 29, 82, 1.0),
+    return Semantics(
+      label: semanticsLabel,
+      button: true,
+      excludeSemantics: true,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: baseStyle?.copyWith(
+          backgroundColor: WidgetStateProperty.all(
+            Color.fromRGBO(46, 29, 82, 1.0),
+          ),
         ),
+        child: icon != null
+            ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(text),
+                    ExcludeSemantics(child: Icon(icon, size: 30)),
+                  ],
+                ),
+              )
+            : Text(text),
       ),
-      child: icon != null
-          ? Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text(text), Icon(icon, size: 30)],
-              ),
-            )
-          : Text(text),
     );
   }
 }
